7d15382eb4b9e19f93f506febe8785ec4b6a7d9c
Ghazal <ghazal.fouladfard@gmail.com>
2020-06-12 03:53:30 +0000

    create a helper functions module and move getUserByEmail into it

--- express_server.js
+++ express_server.js
@@ -4,6 +4,7 @@ const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
 const cookieSession = require('cookie-session')
 const bcrypt = require('bcrypt');
+const helpers = require("./helpers");
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -23,16 +24,6 @@ const users = {
     email: "user@example.com",
     password: "purple-monkey-dinosaur"
   },
-}
-
-//return userID if it finds a user corresponds to the email
-const findUserByEmail = email => {
-  for (user in users) {
-    if (users[user].email === email) {
-      return user;
-    }
-  }
-  return false;
 }
 
 //returns the URLs where the userID is equal to the id of the currently logged-in user.
@@ -125,7 +116,7 @@ app.post("/urls/:shortURL", (req, res) => {
 app.post("/login", (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
-  const userID = findUserByEmail(email);
+  const userID = helpers.getUserByEmail(email,users);
   if (!userID) {
     res.status(403).send("User does not exists!");
   } else {
@@ -158,7 +149,7 @@ app.post("/register", (req, res) => {
   const hashedPassword = bcrypt.hashSync(password, 10);
   if (((email.length === 0) && (password.length === 0))) {
     res.status(400).send("Invalid Email or Password!");
-  } else if (findUserByEmail(email)) {
+  } else if (helpers.getUserByEmail(email,users)) {
     res.status(400).send("Email already exists!");
   }
   const id = generateRandomString();
